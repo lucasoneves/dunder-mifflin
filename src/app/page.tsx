2ecf986138108d653c736a5aec36f0542706c751
lucasoneves <casluhc@gmail.com>
2024-06-03 20:33:58 -0300

    WIP: Grid content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,57 @@
 import Link from "next/link";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import { Button } from "@mui/material";
 import Box from "@/components/Box";
 
 export default function Home() {
   return (
-      <>
+    <>
       <h2>Dashboard</h2>
-      <Box>
-        <span>79</span>
-        <h3>Total Sales</h3>
-      </Box>
-      <Box>
-        <span>5</span>
-        <h3>In progress</h3>
-      </Box>
-      <Box>
-        <span>74</span>
-        <h3>Finished</h3>
-      </Box>
-      <Box>
-        <span>3</span>
-        <h3>New clients</h3>
-      </Box>
-      <Box>
-        <h3>Top Sales</h3>
-        
-      </Box>
-      </>
-      
+      <div className={styles["grid-dashboard"]}>
+        <section className={styles["section-dashboard"]}>
+          <article className={styles['positions-info']}>
+            <Box classes={`${styles["box"]}`}>
+              <span className="divider"></span>
+              <div className="info">
+                <span>79</span>
+                <h3>Total Sales</h3>
+              </div>
+            </Box>
+            <Box classes={`${styles["box"]}`}>
+              <span className="divider"></span>
+              <div className="info">
+                <span>79</span>
+                <h3>In progress</h3>
+              </div>
+            </Box>
+            <Box classes={`${styles["box"]}`}>
+              <span className="divider"></span>
+              <div className="info">
+                <span>79</span>
+                <h3>Finished</h3>
+              </div>
+            </Box>
+            <Box classes={`${styles["box"]}`}>
+              <span className="divider"></span>
+              <div className="info">
+                <span>79</span>
+                <h3>New Clients</h3>
+              </div>
+            </Box>
+          </article>
+          <Box classes={`${styles["box"]}`}>
+            <h3>Top Sales</h3>
+          </Box>
+        </section>
+        <section className={styles["employers-applications"]}>
+          <Box classes={`${styles["box"]}`}>
+            <h3>Employer by Department</h3>
+          </Box>
+          <Box classes={`${styles["box"]}`}>
+            <h3>Recent Application</h3>
+          </Box>
+        </section>
+      </div>
+    </>
   );
 }
--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./box.module.scss";
 
-export default function Box({ children }: { children: React.ReactNode }) {
-  return <div className={styles["box"]}>{children}</div>;
+export default function Box({ children, classes }: { children: React.ReactNode, classes: string }) {
+  return <div className={`${styles['box']} ${[classes]}`}>{children}</div>;
 }
